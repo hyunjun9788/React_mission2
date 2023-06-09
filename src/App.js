e111d9e2b4ac1b13ccabace1d05a80b08541035c
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-09 15:53:45 +0900

    feat: 폰트 크기 및 색상 변경

--- src/App.js
+++ src/App.js
@@ -16,21 +16,21 @@ function App() {
                 <tbody>
                 <tr>
                     <td>1</td>
-                    <td className='content1'>친절한 대응 감사합니다!</td>
-                    <td>익명</td>
-                    <td>2023-05-27</td>
+                    <td className='content'>친절한 대응 감사합니다!</td>
+                    <td className='name'>익명</td>
+                    <td className='date'>2023-05-27</td>
                 </tr>
                 <tr>
                     <td>2</td>
-                    <td className='content2'>질문입니다.</td>
-                    <td>익명</td>
-                    <td>2023-05-29</td>
+                    <td className='content'>질문입니다.</td>
+                    <td className='name'>익명</td>
+                    <td className='date'>2023-05-29</td>
                 </tr>
                 <tr>
                     <td>3</td>
-                    <td className='content3'>배송 문의 드립니다.</td>
-                    <td>익명</td>
-                    <td>2023-06-07</td>
+                    <td className='content'>배송 문의 드립니다.</td>
+                    <td className='name'>익명</td>
+                    <td className='date'>2023-06-07</td>
                 </tr>
                 </tbody>
             </table>
